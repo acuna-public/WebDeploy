8b9d01e782f5c72dc8eec6f6ab26d484c2537686
Acuna <acuna.public@gmail.com>
2021-07-14 19:02:01 +0300

    Update Deployment.php

--- Deployment.php
+++ Deployment.php
@@ -96,7 +96,7 @@
           
           if (!$dryRun) {
             
-            try {$this->deploy->logger->message ($this->deploy->git->readFile ($this->deploy->get ('repository'), $file->get ('name')));
+            try {
               $this->writeFile ($file->get ('name'), $this->deploy->git->readFile ($this->deploy->get ('repository'), $file->get ('name')));
             } catch (\StorageException $e) {
               
